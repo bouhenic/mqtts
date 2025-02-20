caa1f3de97961b78c8536ec89f8df120d67bb9b6
bouhenic <39024540+bouhenic@users.noreply.github.com>
2025-02-20 15:04:18 +0100

    Create config.h

---
+++ ESP32/config.h
@@ -0,0 +1,12 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define WIFI_SSID "YourSSID"
+#define WIFI_PASSWORD "YourPassword"
+#define MQTT_SERVER "your.mqtt.server"
+#define MQTT_PORT 8883
+#define MQTT_USER "your_mqtt_user"
+#define MQTT_PASSWORD "your_mqtt_password"
+#define CA_CERT "-----BEGIN CERTIFICATE-----\n...\n-----END CERTIFICATE-----\n"
+
+#endif
