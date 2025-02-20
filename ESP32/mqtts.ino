16ea442d671802f3e17ebcc474fa381dd622ad9f
bouhenic <39024540+bouhenic@users.noreply.github.com>
2025-02-20 16:01:09 +0100

    Update mqtts.ino

--- ESP32/mqtts.ino
+++ ESP32/mqtts.ino
@@ -1,14 +1,56 @@
 #include <WiFi.h>
 #include <WiFiClientSecure.h>
 #include <PubSubClient.h>
-#include "config.h"  // Fichier séparé contenant les informations sensibles
+
+const char* ssid = "xxxxxxxxxx"; //Compléter ici
+const char* password = "xxxxxxxxxx"; //Compléter ici
+const char* mqtt_server = "5exxxxxxxxxxxxxxxxxxxxx75efd80fd.s1.eu.hivemq.cloud"; //Compléter ici
+const int mqtt_port = 8883;
+const char* mqtt_user = "xxxxxxxxxx"; //Compléter ici
+const char* mqtt_pass = "xxxxxxxxxx"; //Compléter ici
+
+
+// Certificat racine ISRG Root X1 (Let's Encrypt)
+const char* ca_cert = \
+"-----BEGIN CERTIFICATE-----\n" \
+"MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw\n" \
+"TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh\n" \
+"cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4\n" \
+"WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu\n" \
+"ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY\n" \
+"MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc\n" \
+"h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+\n" \
+"0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U\n" \
+"A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW\n" \
+"T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH\n" \
+"B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC\n" \
+"B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv\n" \
+"KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn\n" \
+"OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn\n" \
+"jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw\n" \
+"qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI\n" \
+"rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV\n" \
+"HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq\n" \
+"hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL\n" \
+"ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ\n" \
+"3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK\n" \
+"NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5\n" \
+"ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur\n" \
+"TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC\n" \
+"jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc\n" \
+"oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq\n" \
+"4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA\n" \
+"mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d\n" \
+"emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=\n" \
+"-----END CERTIFICATE-----\n";
+
 
 WiFiClientSecure wifiClient;
 PubSubClient client(wifiClient);
 
 void connectToWiFi() {
   Serial.println("Connexion au WiFi...");
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -19,9 +61,9 @@ void connectToWiFi() {
 void connectToMQTT() {
   while (!client.connected()) {
     Serial.println("Connexion au broker MQTT...");
-    if (client.connect("ESP32Client", MQTT_USER, MQTT_PASSWORD)) {
+    if (client.connect("ESP32Client", mqtt_user, mqtt_pass)) {
       Serial.println("Connecté au broker MQTT");
-      client.subscribe("test/fecamp");
+      client.subscribe("xxxxx/xxxxxx"); //Compléter ici le topic
     } else {
       Serial.print("Échec de connexion, rc=");
       Serial.println(client.state());
@@ -33,6 +75,7 @@ void connectToMQTT() {
 void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message reçu sur le sujet : ");
   Serial.println(topic);
+
   Serial.print("Message : ");
   for (unsigned int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
@@ -42,10 +85,13 @@ void callback(char* topic, byte* payload, unsigned int length) {
 
 void setup() {
   Serial.begin(115200);
+
   connectToWiFi();
-  wifiClient.setCACert(CA_CERT); // Configure le certificat pour la sécurité
-  client.setServer(MQTT_SERVER, MQTT_PORT); 
+
+  wifiClient.setCACert(ca_cert); // Configure le certificat pour la sécurité
+  client.setServer(mqtt_server, mqtt_port); 
   client.setCallback(callback);
+
   connectToMQTT();
 }
 
